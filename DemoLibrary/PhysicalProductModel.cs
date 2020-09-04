c0d7915bbc84e9127769ff6e5a6f866aa118dc94
Tony Watkins <tony@llifion.net>
2020-09-04 11:48:51 +0100

    Inherited base classes must come before interface implementation declarations

--- DemoLibrary/PhysicalProductModel.cs
+++ DemoLibrary/PhysicalProductModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    public class PhysicalProductModel : IProductModel
+    public class PhysicalProductModel : PhysicalProductBase, IProductModel
     {
         public string Title { get; set; }
         public bool HasOrderBeenCompleted { get; private set; }
