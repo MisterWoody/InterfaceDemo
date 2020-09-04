9a5a44b5d6d34e7a709b43a8c97b2707794808ab
Tony Watkins <tony@llifion.net>
2020-09-04 11:34:19 +0100

    Update our driver program to depend on the interface not the concrete implementation of the physical product model

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            List<PhysicalProductModel> cart = AddSampleData();
+            List<IProductModel> cart = AddSampleData();
             CustomerModel customer = GetCustomer();
 
             foreach (PhysicalProductModel prod in cart)
@@ -34,9 +34,9 @@ namespace ConsoleUI
             };
         }
 
-        private static List<PhysicalProductModel> AddSampleData()
+        private static List<IProductModel> AddSampleData()
         {
-            List<PhysicalProductModel> output = new List<PhysicalProductModel>();
+            List<IProductModel> output = new List<IProductModel>();
 
             output.Add(new PhysicalProductModel { Title = "Nerf Football" });
             output.Add(new PhysicalProductModel { Title = "IAmTimCorey T-Shirt" });
